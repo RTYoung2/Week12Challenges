95fd71d1a254c892db11a9644fa39e3aa3dee291
Robert Young <robertyoung@MacBook-Pro-3.local>
2020-12-08 23:26:58 -0500

    commit

--- easy.js
+++ easy.js
@@ -21,3 +21,12 @@
 //   var lines = contents.toString().split("\n").length - 1;
 //   console.log(lines);
 // });
+
+// var fs = require("fs");
+// var path = require("path");
+
+// fs.readdir(process.argv[2], function (err, list) {
+//   list.forEach(function (file) {
+//     if (path.extname(file) === "." + process.argv[3]) console.log(file);
+//   });
+// });
